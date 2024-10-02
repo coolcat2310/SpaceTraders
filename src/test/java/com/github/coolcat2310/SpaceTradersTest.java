2938dd4aa63d98c437007d7bb0fa3271beb89928
coolcat2310 <yunisbukhari@gmail.com>
2024-10-02 21:43:37 +0100

    V0.0 - Initialise Repo

---
+++ src/test/java/com/github/coolcat2310/SpaceTradersTest.java
@@ -0,0 +1,21 @@
+package com.github.coolcat2310;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class SpaceTradersTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
